2598ccf96503246abec9cc7a3f0d27056316e160
kevna <aam13@aber.ac.uk>
2016-04-13 18:52:49 +0100

    Add data model elements

---
+++ MIST/ChannelItem.cs
@@ -0,0 +1,140 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Microsoft.Xna.Framework.Audio;
+
+namespace SurfaceApplicationMist
+{
+    public class ChannelItem
+    {
+
+        /// <summary>
+        /// Internal variable for Volume property.
+        /// </summary>
+        private float VolumeLevel;
+
+        /// <summary>
+        /// Volume property with limit enforcement on mutator (setter).
+        /// </summary>
+        public float Volume
+        {
+            // Simple accessor, we don't need to do anything special getting the volume level
+            get { return VolumeLevel; }
+
+            // Mutator with limits for volume level (can't be over 1 (100%) or under 0 (0%))
+            set
+            {
+                // Upper limit 1.0f (100%)
+                if (value > 1.0f)
+                {
+                    value = 1.0f;
+                }
+                // Lower limit 0.0f (0%)
+                else if (value < 0.0f)
+                {
+                    value = 0.0f;
+                }
+
+                // Set the volume level
+                VolumeLevel = value;
+            }
+        }
+
+        /// <summary>
+        /// Internal variable for Balance property.
+        /// </summary>
+        private float BalanceValue;
+
+        /// <summary>
+        /// Balance property with limit enforcement on mutator (setter).
+        /// </summary>
+        public float Balance
+        {
+            // Simple accessor, we don't need to do anything special getting the balance value
+            get { return BalanceValue; }
+
+            // Mutator with limits for balance value (can't be over 1 (full right) or under -1 (full left))
+            set
+            {
+                // Upper limit 1.0f (full right)
+                if (value > 1.0f)
+                {
+                    value = 1.0f;
+                }
+                // Lower limit -1.0f (full left)
+                else if (value < -1.0f)
+                {
+                    value = -1.0f;
+                }
+
+                // Set the balance value
+                BalanceValue = value;
+            }
+        }
+
+        /// <summary>
+        /// Song object for the soundbyte connected to this channel.
+        /// </summary>
+        protected SoundEffect ChannelSoundbyte;
+
+        /// <summary>
+        /// List of times to start channel playback.
+        /// </summary>
+        protected List<TimeSpan> ChannelPlaybackTimes;
+
+        /// <summary>
+        /// Create ChannelItem without attaching a soundbyte.
+        /// </summary>
+        public ChannelItem()
+        {
+            Volume = 1.0f;
+            Balance = 0.0f;
+            ChannelSoundbyte = null;
+        }
+
+        /// <summary>
+        /// Create ChannelItem initialised with a soundbyte.
+        /// </summary>
+        /// <param name="FileName">Name of the soundbyte file to load.</param>
+        public ChannelItem(String FileName)
+        {
+            Volume = 1.0f;
+            Balance = 0.0f;
+            LoadSoundbyte(FileName);
+        }
+
+        /// <summary>
+        /// Load a soundbyte file into this channel item.
+        /// </summary>
+        /// <param name="FileName">Name of the soundbyte file to load.</param>
+        public void LoadSoundbyte(String FileName)
+        {
+            // Load the soundbyte into the channel item
+            ChannelSoundbyte = Content.Load<SoundEffect>(FileName);
+        }
+
+        /// <summary>
+        /// Playback the soundbyte in this channel (if there is a soundbyte to play).
+        /// 
+        /// Returns Boolean false rather than throwing error if playback is unsuccessful
+        /// so that playback can occur normally if players have channels without assigned soundbytes.
+        /// </summary>
+        /// <param name="Volume">The volume to playback the current soundbyte.</param>
+        /// <param name="Pan">The balance to use playing back the current soundbyte.</param>
+        /// <returns>True if playing back successfully, false in case of error</returns>
+        public Boolean Play()
+        {
+            // Don't have soundbyte to play back
+            if (null == ChannelSoundbyte)
+            {
+                // Sound is not playing if there's no sound to play.
+                return false;
+            }
+
+            // SoundEffect.Play returns true if playing correctly, else false
+            return ChannelSoundbyte.Play(Volume, 0.0f, Balance);
+        }
+
+    }
+}
---
+++ MIST/ChannelList.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace SurfaceApplicationMist
+{
+    class ChannelList
+    {
+
+        protected List<ChannelItem> Channels;
+
+        /// <summary>
+        /// Add a new channel item to the list.
+        /// </summary>
+        /// <param name="Channel">ChannelItem object to add to the list.</param>
+        /// <returns>Index for the newly added channel.</returns>
+        public int AddChannel(ChannelItem Channel)
+        {
+            // Add the new ChannelItem to the channel list
+            Channels.Add(Channel);
+
+            // Quickest method to get the index of the newly added item is to return count-1
+            // as List.add always adds to the end of the list (O(1) is better than O(n)).
+            return (Channels.Count - 1);
+        }
+
+        /// <summary>
+        /// Add a new channel item to the list.
+        /// 
+        /// Overloaded to add a new channel using the filename argument.
+        /// </summary>
+        /// <param name="FileName">Name of the soundbyte file to load.</param>
+        /// <returns>Index for the newly added channel.</returns>
+        public int AddChannel(String FileName)
+        {
+            return AddChannel(new ChannelItem(FileName));
+        }
+
+        /// <summary>
+        /// Add a new channel item to the list.
+        /// 
+        /// Overloaded to add a new blank channel.
+        /// </summary>
+        /// <returns>Index for the newly added channel.</returns>
+        public int AddChannel()
+        {
+            return AddChannel(new ChannelItem());
+        }
+
+        /// <summary>
+        /// Get the ChannelItem from the list by index.
+        /// 
+        /// Allows accessing the channels in the model to update them.
+        /// </summary>
+        /// <param name="ChannelID">Index of the ChannelItem to load.</param>
+        /// <returns>ChannelItem at given index.</returns>
+        public ChannelItem GetChannel(int ChannelID)
+        {
+            return Channels[ChannelID];
+        }
+
+    }
+}
